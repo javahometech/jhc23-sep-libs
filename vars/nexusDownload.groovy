b292bd0bf217c7cac7a6df65922b01b51fe054a8
javahometech <hari.kammana@gmail.com>
2023-09-27 08:53:50 +0530

    Update nexusDownload.groovy

--- vars/nexusDownload.groovy
+++ vars/nexusDownload.groovy
@@ -2,7 +2,7 @@
 
 def call(version,outputFile,repoName,groupId,artifactId, extension){
   def NEXUS_URUL = "http://172.31.42.1:8081"
-  groupId = groupId.replaceAll(".","/")
+  groupId = groupId.replace(".","/")
   echo groupId
   sh """
     curl -u admin:admin -o ${outputFile} -X GET "${NEXUS_URUL}/repository/${repoName}/${groupId}/${artifactId}/${version}/doctor-online-${version}.${extension}"
