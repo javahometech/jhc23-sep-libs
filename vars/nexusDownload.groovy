364d142dea686a9c0a7cddce1c7cf5a93357597c
javahometech <hari.kammana@gmail.com>
2023-09-27 08:35:38 +0530

    Update nexusDownload.groovy

--- vars/nexusDownload.groovy
+++ vars/nexusDownload.groovy
@@ -1,7 +1,7 @@
 
 
 def call(version){
-  def NEXUS_URUL = "http://13.232.5.119:8081"
+  def NEXUS_URUL = "http://172.31.42.1:8081"
   sh """
        curl -u admin:admin -o doctor-online.war -X GET "${NEXUS_URUL}/repository/doctor-online-release/in/javahome/doctor-online/${version}/doctor-online-${version}.war"
   """
